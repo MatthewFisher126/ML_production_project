ab8ef846dfdedf735e58fa35c16e3b91572fc1d1
MatthewFisher126 <matthew.fisher126@gmail.com>
2024-09-17 08:44:46 -0600

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -38,7 +41,7 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            
+
             
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
@@ -57,3 +60,8 @@ if __name__=="__main__":
 
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data, test_data)
+
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
